c734329c9538ee51496c9eab2fd1840c16549470
彭正发 <pengzhengfa@biosan.cn>
2021-02-26 15:45:01 +0800

    添加acme的样本登记接口测试用例

---
+++ src/test/java/util/data/BarCode.java
@@ -0,0 +1,25 @@
+package util.data;
+import java.util.Random;
+/**
+ * @author pengzhengfa
+ */
+public class BarCode {
+    
+    private static Random random = new Random(System.currentTimeMillis());
+    
+    private static String uuid;
+    
+    /**
+     * 生成条形码
+     * @return
+     */
+    public static String barCode(){
+        for (int j = 0; j < 1; j++) {
+            for (int i = 0; i < 15; i++) {
+                int subNum = random.nextInt(9);
+                uuid += subNum;
+            }
+        }
+        return uuid;
+    }
+}
